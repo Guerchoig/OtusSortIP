396a362d03ea31c9b7e26a8c8694a39381bde44b
guerchoig <guerchoig@gmail.com>
2024-01-21 19:09:37 +0300

    Ver 11

--- ip_filter.cpp
+++ ip_filter.cpp
@@ -5,7 +5,8 @@
 #include <vector>
 #include <string>
 #include <sstream>
-#include <algorithm>
+// #include <algorithm>
+#include <cassert>
 
 constexpr std::size_t nof_addr_groups = 4;
 constexpr unsigned int _256_ = 256;
@@ -15,7 +16,7 @@ constexpr unsigned char FILT_ADR3 = 70;
 constexpr unsigned char FILT_ADR4 = 46;
 
 // A fixed size int array represents an IP address
-using ip_addr_t = std::array<unsigned char, nof_addr_groups>;
+using ip_addr_t = std::vector<unsigned char>; // size = nof_addr_groups
 
 // The type of the whole list of IP addresses
 // is a vector of fixed size arrays
@@ -33,12 +34,12 @@ void fill_the_pool(pool_t &res)
         // std::cin.rdbuf(in.rdbuf()); // redirect std::cin to input/ip_filter.tsv
         for (std::string line; std::getline(std::cin, line);)
         {
-            ip_addr_t v{0};
-            ip_addr_t::iterator i = v.begin();
+            ip_addr_t v(nof_addr_groups);
+            auto i = 0;
             for (std::string::size_type start = 0; start != std::string::npos; ++i)
             {
                 auto stop = line.find_first_of("\t.", start);
-                *i = std::stoi(line.substr(start, stop - start));
+                v.at(i) = std::stoi(line.substr(start, stop - start));
                 if (line.at(stop) == '\t')
                     break;
                 start = stop + 1;
@@ -52,7 +53,7 @@ void fill_the_pool(pool_t &res)
     }
 }
 
-// Folds up all uchar (binary) parts of an IP address into a 4-bytes int
+// Folds up all uchar (binary) chunks of an IP address into a 4-bytes int
 // to enable comparison
 inline unsigned int make_int(const ip_addr_t &addr)
 {
@@ -62,7 +63,7 @@ inline unsigned int make_int(const ip_addr_t &addr)
     return res;
 }
 
-// Console-outputs an IP-address vector
+// Outputs an IP-address vector to cout
 void output_address(const ip_addr_t &addr)
 {
     std::stringstream ss;
@@ -76,7 +77,7 @@ void output_address(const ip_addr_t &addr)
 }
 
 // Outputs the whole vector of IP-addresses,
-// for each of provided lambda-ed output filters
+// for each of provided filters
 template <typename... Lambda>
 void multi_output_ip_pool(Lambda &&...filters)
 {
